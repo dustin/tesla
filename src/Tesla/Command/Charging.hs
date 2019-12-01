aedaa5330b06364a519ed34cce81bead679c0ed5
Dustin Sallings <dustin@spy.net>
2019-11-30 22:45:34 -0800

    Open and close the charge port door.

--- src/Tesla/Command/Charging.hs
+++ src/Tesla/Command/Charging.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Tesla.Command.Charging (
-  startCharging, stopCharging, setLimit
+  startCharging, stopCharging, setLimit, openChargePort, closeChargePort
   ) where
 
 import           Control.Monad.IO.Class (MonadIO (..))
@@ -17,3 +17,9 @@ stopCharging = runCmd' "charge_stop"
 
 setLimit :: MonadIO m => Int -> Car m CommandResponse
 setLimit to = runCmd "set_charge_limit" ["percent" := to ]
+
+openChargePort :: MonadIO m => Car m CommandResponse
+openChargePort = runCmd' "charge_port_door_open"
+
+closeChargePort :: MonadIO m => Car m CommandResponse
+closeChargePort = runCmd' "charge_port_door_close"
