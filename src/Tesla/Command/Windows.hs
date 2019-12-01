f0d686a212c8bb18682661833756957748587743
Dustin Sallings <dustin@spy.net>
2019-11-30 22:42:56 -0800

    Added sunroof control to windows

--- src/Tesla/Command/Windows.hs
+++ src/Tesla/Command/Windows.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Tesla.Command.Windows (
-  ventWindows, closeWindows
+  ventWindows, closeWindows, ventSunroof, closeSunroof
   ) where
 
 import           Control.Monad.IO.Class (MonadIO (..))
@@ -17,3 +17,12 @@ ventWindows = windowControl "vent" (0,0)
 
 closeWindows :: MonadIO m => (Double, Double) -> Car m CommandResponse
 closeWindows = windowControl "close"
+
+sc :: MonadIO m => Text -> Car m CommandResponse
+sc c = runCmd "sun_roof_control" [ "state" := c ]
+
+ventSunroof :: MonadIO m => Car m CommandResponse
+ventSunroof = sc "vent"
+
+closeSunroof :: MonadIO m => Car m CommandResponse
+closeSunroof = sc "close"
