1b3acc29026038d58d02ef917e2422002e4deb40
Dustin Sallings <dustin@spy.net>
2019-11-30 22:23:02 -0800

    Added SpeedLimit controls.

---
+++ src/Tesla/Command/SpeedLimit.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Tesla.Command.SpeedLimit (
+  speedLimit, activateSpeedLimit, deactivateSpeedLimit, clearSpeedLimitPIN
+  ) where
+
+import           Control.Monad.IO.Class (MonadIO (..))
+import           Network.Wreq           (FormParam (..))
+import           Tesla.Command
+
+speedLimit :: MonadIO m => Int -> Car m CommandResponse
+speedLimit to = runCmd "speed_limit_set_limit" ["limit_mph" := to ]
+
+speedo :: MonadIO m => String -> Int -> Car m CommandResponse
+speedo c pin = runCmd c ["pin" := pin ]
+
+activateSpeedLimit :: MonadIO m => Int -> Car m CommandResponse
+activateSpeedLimit = speedo "speed_limit_activate"
+
+deactivateSpeedLimit :: MonadIO m => Int -> Car m CommandResponse
+deactivateSpeedLimit = speedo "speed_limit_deactivate"
+
+clearSpeedLimitPIN :: MonadIO m => Int -> Car m CommandResponse
+clearSpeedLimitPIN = speedo "speed_limit_clear_pin"
