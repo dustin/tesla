f442fd50e7851e756855a7f8279e889916ef84ba
Dustin Sallings <dustin@spy.net>
2019-12-01 10:44:37 -0800

    Rename the software update commands.
    And use TH to make the simple one just because.

--- src/Tesla/Command/Software.hs
+++ src/Tesla/Command/Software.hs
@@ -1,18 +1,20 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
 
 module Tesla.Command.Software (
-  schedule, cancel
+  scheduleUpdate, cancelUpdate
   ) where
 
 import           Control.Monad.IO.Class (MonadIO (..))
 import           Network.Wreq           (FormParam (..))
 
 import           Tesla.Command
+import           Tesla.Command.TH
 
 -- | Schedule a software update in this many seconds.
-schedule :: MonadIO m => Int -> Car m CommandResponse
-schedule secs = runCmd "schedule_software_update" ["offset_sec" := secs]
+scheduleUpdate :: MonadIO m => Int -> Car m CommandResponse
+scheduleUpdate secs = runCmd "schedule_software_update" ["offset_sec" := secs]
 
 -- | Cancel a scheduled software update.
-cancel :: MonadIO m => Car m CommandResponse
-cancel = runCmd' "cancel_software_update"
+carCMD "cancelUpdate" "cancel_software_update"
+
