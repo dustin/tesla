e0115a3e86cbbe2d287c1ee48263cd7adbf7c7e6
Dustin Sallings <dustin@spy.net>
2021-11-21 12:35:48 -0800

    Allow modification of scheduled charging.

--- src/Tesla/Car/Command/Charging.hs
+++ src/Tesla/Car/Command/Charging.hs
@@ -3,7 +3,8 @@
 
 module Tesla.Car.Command.Charging (
   startCharging, stopCharging, setLimit, openChargePort, closeChargePort,
-  setAmps
+  setAmps,
+  scheduledChargingOff, scheduleCharging
   ) where
 
 import           Control.Monad.IO.Class (MonadIO (..))
@@ -18,6 +19,14 @@ setLimit to = runCmd "set_charge_limit" ["percent" := to ]
 -- | Set the charge current.
 setAmps :: MonadIO m => Int -> Car m CommandResponse
 setAmps to = runCmd "set_charging_amps" ["charging_amps" := to]
+
+-- | Disable scheduled charging.
+scheduledChargingOff :: MonadIO m => Car m CommandResponse
+scheduledChargingOff = runCmd "set_scheduled_charging" [ "enable" := False ]
+
+-- | Schedule charging for the given number of minutes after midnight (local time).
+scheduleCharging :: MonadIO m => Int -> Car m CommandResponse
+scheduleCharging mins = runCmd "set_scheduled_charging" [ "enable" := True, "time" := mins ]
 
 mkNamedCommands [("startCharging", "charge_start"),
                  ("stopCharging", "charge_stop"),
