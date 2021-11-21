3596a266f57f56780f667c63145f3c8f33ba171d
Dustin Sallings <dustin@spy.net>
2021-11-21 12:26:50 -0800

    Add setAmps in charging

--- src/Tesla/Car/Command/Charging.hs
+++ src/Tesla/Car/Command/Charging.hs
@@ -2,7 +2,8 @@
 {-# LANGUAGE TemplateHaskell   #-}
 
 module Tesla.Car.Command.Charging (
-  startCharging, stopCharging, setLimit, openChargePort, closeChargePort
+  startCharging, stopCharging, setLimit, openChargePort, closeChargePort,
+  setAmps
   ) where
 
 import           Control.Monad.IO.Class (MonadIO (..))
@@ -10,8 +11,13 @@ import           Network.Wreq           (FormParam (..))
 
 import           Tesla.Car.Command
 
+-- | Set the desired charge level (percent).
 setLimit :: MonadIO m => Int -> Car m CommandResponse
 setLimit to = runCmd "set_charge_limit" ["percent" := to ]
+
+-- | Set the charge current.
+setAmps :: MonadIO m => Int -> Car m CommandResponse
+setAmps to = runCmd "set_charging_amps" ["charging_amps" := to]
 
 mkNamedCommands [("startCharging", "charge_start"),
                  ("stopCharging", "charge_stop"),
