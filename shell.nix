57e24731b26d38a6f1b7bda627a9270444ac8018
Dustin Sallings <dustin@spy.net>
2023-04-13 10:11:13 -1000

    Update LTS and dev tools

---
+++ shell.nix
@@ -0,0 +1,19 @@
+{ pkgs ? import <nixpkgs> { } }:
+with pkgs;
+mkShell {
+  buildInputs = [
+    stack
+    sqlite-interactive
+    ffmpeg
+    stylish-haskell
+    hlint
+    niv
+    ghcid
+    haskell-language-server
+    haskellPackages.tasty-discover
+  ];
+
+  shellHook = ''
+    # ...
+  '';
+}
