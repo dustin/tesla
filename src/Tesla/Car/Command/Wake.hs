207d1ffdce7808da8857e78e1dc946987331436f
Dustin Sallings <dustin@spy.net>
2023-04-15 14:05:11 -1000

    Sorted language extensions

--- src/Tesla/Car/Command/Wake.hs
+++ src/Tesla/Car/Command/Wake.hs
@@ -1,5 +1,5 @@
-{-# LANGUAGE OverloadedStrings   #-}
-{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications  #-}
 
 module Tesla.Car.Command.Wake (
   wakeUp
