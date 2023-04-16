13de89089ca85f132d209352c8744ca324b8f64b
Dustin Sallings <dustin@spy.net>
2023-04-15 20:37:28 -1000

    Let the caller parse wakeup however they want

--- src/Tesla/Car/Command/Wake.hs
+++ src/Tesla/Car/Command/Wake.hs
@@ -1,14 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TypeApplications  #-}
 
 module Tesla.Car.Command.Wake (
   wakeUp
   ) where
 
-import           Control.Lens
 import           Control.Monad.IO.Class (MonadIO (..))
 import           Data.Aeson
-import           Data.Aeson.Lens
 import qualified Data.ByteString.Lazy   as BL
 
 
@@ -16,7 +13,5 @@ import           Tesla.Car              (currentVehicleID, vehicleURL)
 import           Tesla.Car.Command
 import           Tesla.Internal.HTTP    (jpostAuth)
 
-wakeUp :: MonadIO m => Car m (Maybe Value)
-wakeUp = do
-  v <- currentVehicleID
-  preview (_Just . key "response") <$> jpostAuth @_ @(Maybe Value) (vehicleURL v "wake_up") BL.empty
+wakeUp :: (FromJSON j, MonadIO m) => Car m j
+wakeUp = currentVehicleID >>= \v -> jpostAuth (vehicleURL v "wake_up") BL.empty
